c6f26f6ca7b8ffa08df5a927dec98f9ce272e4c6
Brian Wright <briankw23@gmail.com>
2018-04-04 19:19:07 -0500

    wip event consoling

--- main.js
+++ main.js
@@ -1,36 +1,45 @@
-
-console.log("start of JS", Date.now());
-
 const printToDom = (domString, divId) => {
-    document.getElementById("main").innerHTML = domString;
+    document.getElementById(divId).innerHTML = domString;
     };
     
-    function Xhrload(){
+function xhrload() {
     const data = JSON.parse(this.responseText);
     console.log("data", data);
     domString(data.planets);
-    };
+    addEventTarget();
+};
 
-    const domString = (planetsArray) =>{
-        let domString="";
-        for (let i = 0; i < planetsArray.length; i++) {
-            domString += `<div class="cards">`;
-            domString += `<div> ${planetsArray[i].name} </div>`;
-            domString += `</div>`;
-        }
-        printToDom(domString,"main")
-    };
+const domString = (planetsArray) =>{
+    let domString="";
+    for (let i = 0; i < planetsArray.length; i++) {
+        domString += `<div class="cards" >`;
+        domString += `<p> ${planetsArray[i].name} </p>`;
+        domString += `</div>`;
+    }
+    printToDom(domString,"main")
+};
 
-    function Xhrfail(){
+function xhrfail(){
         console.log("fail");
     }
-    const startApplication = () => {
-        let myRequest = new XMLHttpRequest();
-        myRequest.addEventListener("load", Xhrload);
-        myRequest.addEventListener("error", Xhrfail);
-        myRequest.open("GET", "planets.json");
-        myRequest.send();
-        console.log("myrequest", myRequest);
-    };
-    console.log("start of JS", Date.now());
-    startApplication();
+const addEventTarget = () => {
+    const eTarget = document.getElementsByClassName("cards");
+    for (let i = 0; i < eTarget.length; i++) {
+        eTarget[i].addEventListener("click", showstuff);
+    }
+}
+    
+const showstuff = (e) => {
+    console.log("i clicked that");
+}
+
+const startApplication = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener("load", xhrload);
+    myRequest.addEventListener("error", xhrfail);
+    myRequest.open("GET", "planets.json");
+    myRequest.send();
+    console.log("myrequest", myRequest);
+};
+
+startApplication();
