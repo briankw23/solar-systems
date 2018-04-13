22ef050bdbd273a294f88219c202ac8bdf131239
Brian Wright <briankw23@gmail.com>
2018-04-13 16:34:13 -0500

    search bar working

--- main.js
+++ main.js
@@ -20,11 +20,6 @@ const planetClick = (e) => {
         eTarget[i].addEventListener("click", http2);
     }
 };
-const showPlanets = (e) => {
-    const button = document.getElementById("allPlanets");
-        button.addEventListener("click", httpI);
-};
-
 const planetCardBuilder = (data, target) => {
     console.log(data);
     console.log(target);
@@ -34,20 +29,61 @@ const planetCardBuilder = (data, target) => {
             domString2 += `<div class"large">`;
             domString2 += `<h2 class="text head line">${data.planets[i].name}</h2>`;
             domString2 += `<button class="line" type="button" id="allPlanets">X</button>`;
-            domString2 +=`<div>`
+            domString2 +=`<div id="im">`
             domString2 += `<img class="image2" src="${data.planets[i].imageUrl}">`;
             domString2 +=`</div>`
             domString2 += `<p class="text">${data.planets[i].description}</p>`;
             domString2 += `<p class="text">${data.planets[i].isGasey}</p>`;
             domString2 += `<p class="text">${data.planets[i].numberofmoons}</p>`;
             domString2 += `<p class="text">${data.planets[i].nameoflargestmoon}</p>`;
-            
             domString2 += `</div>`;
             printToDom(domString2,"main");
             showPlanets();
         }
     }
 }
+const showPlanets = (e) => {
+    const button = document.getElementById("allPlanets");
+        button.addEventListener("click", http3);
+};
+function http3 (e) {
+    const http = new XMLHttpRequest();
+        http.onreadystatechange = function() { 
+            if (http.readyState == 4 && http.status == 200){
+                const data = JSON.parse(http.responseText);
+                console.log("http3:", data);
+                let searchInput = document.getElementById('input');
+                planetSearchBuilder(data, searchInput.value);
+                console.log(e);
+            }  
+            else {
+                console.log("status", http.status);
+            }
+        }
+    http.open("GET", "planets.json");
+    http.send();
+    console.log(http);
+};
+const searchBarTarget = (e) => {
+    const search = document.getElementById('input');
+    search.addEventListener("keypress",http3);
+
+};
+
+const planetSearchBuilder = (data, target) => {
+console.log(target);
+let domString3="";
+
+for (let i = 0; i < data.planets.length; i++) {
+    if(target.toLowerCase() === data.planets[i].name.toLowerCase()){
+    domString3 += `<div class="cards">`;
+    domString3 += `<p data-set="${data.planets[i].name}">${data.planets[i].name}</p>`;
+    domString3 +=`<img class="image" src="${data.planets[i].imageUrl}">`;
+    domString3 +=`</div>`;      
+    printToDom(domString3,"main");
+    }
+    }
+};
 function http2 (e) {
     const http = new XMLHttpRequest();
         http.onreadystatechange = function() { 
@@ -85,5 +121,6 @@ function httpI () {
 const startApplication = () => {    
     httpI();
     planetClick();
+    searchBarTarget();
 };
 startApplication();
